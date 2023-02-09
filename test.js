1b36327767a9c4d333f186aff46942e73cdfd857
ronykris <rony.kris@gmail.com>
2023-02-09 18:57:35 +0530

    check version

---
+++ test.js
@@ -0,0 +1,4 @@
+import { bacalhauSD, checkVersion } from './utils.js'
+
+checkVersion()
+bacalhauSD('a cat basking on a silver mountain in the backdrop of a bright star studded sky')
--- utils.js
+++ utils.js
@@ -27,27 +27,53 @@ function downloadClient() {
     return shell.exec("echo $?").code
 }
 
-//invoke bacalhau SD command
-export function bacalhauSD(prompt) {
+export const checkVersion = () => {
+    const cmd = 'bacalhau version'
+    const versions = shell.exec(cmd).stdout.trim().split('\n')
+    var download = 0;
+
+    if (versions[0].split(':')[1].trim() === versions[1].split(':')[1].trim()) { download = 0 } else { download = 1 }
+
+    console.log('download:', download)
+    return download
+}
+
+const submitJob = (prompt) => {
     try {
+        const submitSDJob = `bacalhau docker run --gpu 1 ghcr.io/bacalhau-project/examples/stable-diffusion-gpu:0.0.1 -- python main.py --o ./outputs --p "${prompt}" --n 35`
+
+        const data = []
+        var obj = new Object()
+
+        const jobId = extract(fetchJobId, shell.exec(submitSDJob).stdout)
+        const cid = extract(fetchCID, shell.exec(`bacalhau describe ${jobId}`).stdout)
+        console.log(`CID : ${cid}`)
+        const url = `https://ipfs.io/ipfs/${cid}/outputs/image0.png`
+
+        obj.jobID = jobId
+        obj.CID = cid
+        obj.url = url
+        data.push(obj)
+        console.log(data)
+        return data;
+    } catch (e) {
+        console.error(e)
+    }
+
+}
+
+//invoke bacalhau SD command
+export async function bacalhauSD(prompt) {
+    try {
+
         if (prompt !== undefined) {
             console.log(`prompt : ${prompt}`)
-            const submitSDJob = `bacalhau docker run --gpu 1 ghcr.io/bacalhau-project/examples/stable-diffusion-gpu:0.0.1 -- python main.py --o ./outputs --p "${prompt}" --n 35`
-
-            const data = []
-            var obj = new Object()
-
-            const jobId = extract(fetchJobId, shell.exec(submitSDJob).stdout)
-            const cid = extract(fetchCID, shell.exec(`bacalhau describe ${jobId}`).stdout)
-            console.log(`CID : ${cid}`)
-            const url = `https://ipfs.io/ipfs/${cid}/outputs/image0.png`
-
-            obj.jobID = jobId
-            obj.CID = cid
-            obj.url = url
-            data.push(obj)
-            console.log(data)
-            return data;
+            if (checkVersion() === 1) {
+                downloadClient()
+                return submitJob(prompt)
+            } else {
+                return submitJob(prompt)
+            }
         } else {
             return 'prompt undefined'
         }
